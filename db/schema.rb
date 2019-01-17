1d2fc0f55686bc1368ca99055d4d825e1f535ab9
Antonino <antoninoscaffidi@gmail.com>
2019-01-17 15:20:48 +0100

    Added field region in points tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_163635) do
+ActiveRecord::Schema.define(version: 2019_01_17_135717) do
 
   create_table "points", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_11_20_163635) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "region"
   end
 
 end

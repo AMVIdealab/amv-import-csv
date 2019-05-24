703e628dba899f366c7f8d241231b43d0493dd20
Antonino <antoninoscaffidi@gmail.com>
2019-05-24 17:54:14 +0200

    Updated import method

--- app/models/point.rb
+++ app/models/point.rb
@@ -3,7 +3,7 @@ class Point < ApplicationRecord
 	#validates :user, :steps, :distance, :exercise, :sleep, :calories, presence: true
 
 	def self.import(file)
-		CSV.foreach(file.path, { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+		CSV.foreach(file.path, {encoding: "UTF-8", col_sep: ";", headers: true, header_converters: :symbol, converters: :all}) do |row|
 			if Point.exists?(name: row[:name])
 				puts 'this point already exists'
 			else
